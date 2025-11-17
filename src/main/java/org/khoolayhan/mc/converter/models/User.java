0aee8c7d80d9c22f3fb7206c39f83d0e3385a804
KhooLayHan <khoolayhan@github.com>
2025-11-17 10:53:45 +0800

    feat: Added starter core convertion strategy and data models.

--- src/main/java/org/khoolayhan/mc/converter/models/User.java
+++ src/main/java/org/khoolayhan/mc/converter/models/User.java
@@ -1,11 +1,3 @@
 package org.khoolayhan.mc.converter.models;
 
-public record User(int id, String name, String email) {
-  public User() {}
-
-  public User(int id, String name, String email) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-  }
-}
+public record User(int id, String name, String email) {}
