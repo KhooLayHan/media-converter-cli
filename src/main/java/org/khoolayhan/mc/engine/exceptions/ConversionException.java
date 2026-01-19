05febb88d76b779aa7a88634e4e463959a26be6c
KhooLayHan <khoolayhan@github.com>
2026-01-19 20:33:31 +0800

    feat: Added new MediaConverterException base class.

--- src/main/java/org/khoolayhan/mc/engine/exceptions/ConversionException.java
+++ src/main/java/org/khoolayhan/mc/engine/exceptions/ConversionException.java
@@ -1,7 +1,16 @@
 package org.khoolayhan.mc.engine.exceptions;
 
-public class ConversionException extends Exception {
-    public ConversionException(String message, Throwable cause) {
+/**
+ * Exception thrown when a media conversion operation fails.
+ * This exception indicates that the conversion process encountered an error
+ * during execution, such as file processing issues or codec problems.
+ */
+public class ConversionException extends MediaConverterException {
+	public ConversionException(String message) {
+		super(message);
+	}
+
+	public ConversionException(String message, Throwable cause) {
         super(message, cause);
     }
 }
