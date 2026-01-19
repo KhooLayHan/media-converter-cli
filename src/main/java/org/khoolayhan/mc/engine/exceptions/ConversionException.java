b201313aa3b7f4fc7ff3c9ddd5084f5c64a8ea84
KhooLayHan <khoolayhan@github.com>
2026-01-19 20:43:11 +0800

    feat: Added new MediaConverterException base class.

--- src/main/java/org/khoolayhan/mc/engine/exceptions/ConversionException.java
+++ src/main/java/org/khoolayhan/mc/engine/exceptions/ConversionException.java
@@ -1,16 +1,16 @@
 package org.khoolayhan.mc.engine.exceptions;
 
 /**
- * Exception thrown when a media conversion operation fails.
- * This exception indicates that the conversion process encountered an error
- * during execution, such as file processing issues or codec problems.
+ * Exception thrown when a media conversion operation fails. This exception indicates that the
+ * conversion process encountered an error during execution, such as file processing issues or codec
+ * problems.
  */
 public class ConversionException extends MediaConverterException {
-	public ConversionException(String message) {
-		super(message);
-	}
+    public ConversionException(String message) {
+        super(message);
+    }
 
-	public ConversionException(String message, Throwable cause) {
+    public ConversionException(String message, Throwable cause) {
         super(message, cause);
     }
 }
--- src/main/java/org/khoolayhan/mc/engine/exceptions/UnsupportedFormatException.java
+++ src/main/java/org/khoolayhan/mc/engine/exceptions/UnsupportedFormatException.java
@@ -1,16 +1,16 @@
 package org.khoolayhan.mc.engine.exceptions;
 
 /**
- * Exception thrown when an unsupported file format is encountered.
- * This exception indicates that the requested conversion involves a file format
- * that is not supported by the Media Converter application.
+ * Exception thrown when an unsupported file format is encountered. This exception indicates that
+ * the requested conversion involves a file format that is not supported by the Media Converter
+ * application.
  */
 public class UnsupportedFormatException extends MediaConverterException {
     public UnsupportedFormatException(String message) {
         super(message);
     }
 
-	public UnsupportedFormatException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public UnsupportedFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
